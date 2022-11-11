dc1de5a763fe5750aea2163d76a13d659a59cd55
James Owens <jjo@referenciando.com>
2022-11-11 11:26:29 -0300

    initial commit

---
+++ Tests/Tests.ino
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2022 - James Owens <jjo@arduando.com.br>
+ * 
+ * File:	Tests.ino
+ * Created:	11/11/2022 11:19:10
+ * Source:	
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with panStamp; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
+ * USA.
+ */
+
+
+
+void setup() {
+
+}
+
+
+void loop() {
+  
+}
